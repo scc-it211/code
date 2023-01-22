412b2b30541834631ff756c877fda7cadc4c097d
BC Ko <bcko@users.noreply.github.com>
2023-01-21 17:11:48 -0800

    Create Variable1.java

---
+++ Variable1.java
@@ -0,0 +1,15 @@
+
+class Variable1 {
+    public static void main(String[] args) {
+        int myInteger1;
+        int myInteger2;
+
+        myInteger1 = 1000;
+        System.out.println("myInteger1: " + myInteger1);
+
+        myInteger2 = myInteger1 / 5;
+
+        System.out.print("myInteger2 is myInteger1 / 2: ");
+        System.out.print(myInteger2);
+    }    
+}
