d67688dc76472d7210b15ec6a3ee13b5f9e568d6
BC Ko <bcko@users.noreply.github.com>
2023-01-21 17:11:27 -0800

    Create FirstProgram.java

---
+++ FirstProgram.java
@@ -0,0 +1,10 @@
+/*
+    First Java program
+*/
+class FirstProgram {
+    // A Java program begins with a call to main()
+    public static void main(String[] args) {
+        String output = "My name is BC";
+        System.out.println(output);
+    }
+}
