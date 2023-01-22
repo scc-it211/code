abf539839ea470b746cb6b3d8e14a4a9c2fa9b1b
Byungcheon Ko <bko30@gatech.edu>
2023-01-21 18:53:58 -0800

    FahrenheitToCelsius

---
+++ FahrenheitToCelsius.java
@@ -0,0 +1,17 @@
+
+class FahrenheitToCelsius {
+    public static void main(String[] args) {
+        double fahrenheitDegrees;
+        double celsiusDegrees;
+
+        fahrenheitDegrees = 99.0;
+
+        // convert F to C
+        celsiusDegrees = (fahrenheitDegrees - 32) * 5 / 9;
+
+        System.out.print(
+            fahrenheitDegrees + " degrees in Fahrenheit is " 
+            + celsiusDegrees + " degrees in Celsius");
+
+    }
+}
