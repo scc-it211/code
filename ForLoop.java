5c1d0347236082c6052f205d1f9a28ae58ff571a
Byungcheon Ko <bko30@gatech.edu>
2023-01-21 19:19:47 -0800

    ForLoop

---
+++ ForLoop.java
@@ -0,0 +1,19 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        // count
+        int countStart = 0;
+        int countEnd = 10;
+        
+        System.out.println(
+            "Let me count from " 
+            + countStart + " to "
+            + countEnd
+        );
+
+        // count is the loop control variable.
+        for (int count = countStart; count <= countEnd; count++) {
+            System.out.println(count);
+        }
+        System.out.println("count done");
+    }
+}
