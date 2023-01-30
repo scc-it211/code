d499899a0dd90211ed8c7263f83890317e5428a9
BC Ko <bcko@users.noreply.github.com>
2023-01-30 15:19:50 -0800

    WhileLoop

---
+++ WhileLoop.java
@@ -0,0 +1,9 @@
+class WhileLoop {
+    public static void main(String[] args) {
+        int num = 1;
+        while (num <= 10) {
+            System.out.print(num);
+            num += 1;
+        }
+    }
+}
