58534494a61408c90608b536c61b884f0a6cbfea
BC Ko <bcko@users.noreply.github.com>
2023-02-06 15:55:56 -0800

    update

--- StringSwitch.java
+++ StringSwitch.java
@@ -1,24 +1,22 @@
 public class StringSwitch {
-
     public static void main(String[] args) {
         String command = "run";
-
         switch (command) {
             case "bark":
-            System.out.println("barking");
-            break;
+                System.out.println("barking");
+                break;
 
             case "run":
-            System.out.println("running");
-            break;
+                System.out.println("running");
+                break;
 
             case "sit":
-            System.out.println("sitting");
+                System.out.println("sitting");
+                break;
 
             default:
                 System.out.println("don't know yet");
                 break;
         }
     }
-    
 }
