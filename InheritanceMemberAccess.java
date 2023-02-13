6bb9d26547d2d5b6f2d1f301cb108e36c88cc6a8
BC Ko <bcko@users.noreply.github.com>
2023-02-13 15:41:34 -0800

    InheritanceMemberAccess

---
+++ InheritanceMemberAccess.java
@@ -0,0 +1,30 @@
+class A {
+    int x; // default access
+    private int y;
+
+    void setXY(int a, int b) {
+        x = a;
+        y = b;
+    }
+}
+
+class B extends A {
+    int total;
+
+    void sum() {
+        // y is not accessible
+        total = x + y;
+    }
+}
+
+
+class InheritanceMemberAccess {
+    public static void main(String[] args) {
+        B subClassObject = new B();
+        subClassObject.setXY(11, 13);
+
+        subClassObject.sum();
+        System.out.println("Total: " + subClassObject.total);
+
+    }
+}
