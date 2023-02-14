0c91eac8e493a980db800b787fef09a48b600648
BC Ko <bcko@users.noreply.github.com>
2023-02-13 16:41:08 -0800

    FinalToPreventOverriding.java

--- FinalToPreventOverriding.java
+++ FinalToPreventOverriding.java
@@ -1,15 +1,18 @@
 
 class FinalToPreventOverriding {
     final void printMethod() {
-        System.out.println("This is a final print method");
+        System.out.println(
+            "This is a final print method");
     }
 }
 
-class FinalToPreventOverridingSubclass extends FinalToPreventOverriding {
+class FinalToPreventOverridingSubclass 
+    extends FinalToPreventOverriding {
     /* 
     // cannot override final print method
     void printMethod() {
-       System.out.println("this is a subclass of final print method") 
+       System.out.println(
+        "this is a subclass of final print method") 
     }
     */
 }
