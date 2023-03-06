cc1992c9ff864a56568b42be8b2cb305aa7b6ecb
BC Ko <bcko@users.noreply.github.com>
2023-03-06 13:34:56 -0800

    packages demo

---
+++ FirstDemo/Demo.java
@@ -0,0 +1,7 @@
+package FirstDemo;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("First Demo");
+    }
+}
---
+++ SecondDemo/Demo.java
@@ -0,0 +1,7 @@
+package SecondDemo;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Second Demo");
+    }
+}
