5d9c1871ce10de37d6622e0c79096d8b1efcafec
BC Ko <bcko@users.noreply.github.com>
2023-03-06 16:19:20 -0800

    storage interface

---
+++ storage_example/Backpack.java
@@ -0,0 +1,19 @@
+package storage_example;
+
+class Backpack implements Storage {
+    public void addItem() {
+        // not implemented
+    }
+    public void removeItem() {
+        // not implemented
+
+    }
+    public void printItems() {
+        // not implemented
+
+    }
+    public int getNumItems() {
+        // not implemented
+        return 1;
+    }
+}
---
+++ storage_example/storageInterface.java
@@ -0,0 +1,8 @@
+package storage_example;
+
+interface Storage {
+    public void addItem();
+    public void removeItem();
+    public void printItems();
+    public int getNumItems();
+}
